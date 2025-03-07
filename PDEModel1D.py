f8b71973efb3ca55b3a52f8bd77bbe855071c3d9
ayillah <37383393+ayillah@users.noreply.github.com>
2025-03-07 10:34:05 -0600

    Add files via upload

---
+++ PDEModel1D.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod # ABC = "Abstract base class"
+
+class PDEModel1D(ABC):
+  def __init__(self, numVars): 
+    self.numVars = numVars
+
+
+  def numVars(self):
+    return self.numVars
+
+
+  @abstractmethod
+  def f(self, x):
+    
+    pass
+
+
+  @abstractmethod
+  def applyLeftBC(self, x, t, dx, dt, u_prev):
+    '''
+    Do whatever is needed to obtain u_cur at the left boundary point.
+    
+    Arguments are:
+      *) x -- location of left boundary point
+      *) t -- current time (i.e., time at end of step)
+      *) dx -- grid spacing dx = x_1 - x_0
+      *) dt -- timestep size
+      *) u_prev -- solution at previous timestep 
+    '''
+    pass
+
+  
+  @abstractmethod
+  def applyRightBC(self, x, t, dx, dt, u_prev, nx):
+    '''
+    Do whatever is needed to obtain u_cur at the right boundary point.
+    
+    Arguments are:
+      *) x -- location of right boundary point
+      *) t -- current time (i.e., time at end of step)
+      *) dx -- grid spacing dx = x_{nx} - x_{nx-1}
+      *) dt -- timestep size
+      *) u_prev -- solution at previous timestep 
+    '''
+    pass
+
+  
+  
+
+  
+
+  
